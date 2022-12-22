3a4c2c1cd61b1c016ea3f18982281f0d21576522
devneser <zh_alex@yahoo.com>
2022-12-22 23:59:34 +0800

    refactor: remove crypto

--- test/c/03-hash.js
+++ test/c/03-hash.js
@@ -1,37 +1,36 @@
-var nacl = require('../../' + (process.env.NACL_SRC || 'nacl.min.js'));
-var crypto = require('crypto');
-var spawn = require('child_process').spawn;
-var path = require('path');
-var test = require('tape');
+var nacl = require("../../" + (process.env.NACL_SRC || "nacl.min.js"));
+var spawn = require("child_process").spawn;
+var path = require("path");
+var test = require("tape");
 
 function chash(msg, callback) {
-  var p = spawn(path.resolve(__dirname, 'chash'));
+  var p = spawn(path.resolve(__dirname, "chash"));
   var result = [];
-  p.stdout.on('data', function(data) {
+  p.stdout.on("data", function (data) {
     result.push(data);
   });
-  p.on('close', function(code) {
-    return callback(Buffer.concat(result).toString('utf8'));
+  p.on("close", function (code) {
+    return callback(Buffer.concat(result).toString("utf8"));
   });
-  p.on('error', function(err) {
+  p.on("error", function (err) {
     throw err;
   });
   p.stdin.write(msg);
   p.stdin.end();
 }
 
-test('nacl.hash (C)', function(t) {
+test("nacl.hash (C)", function (t) {
   function check(num) {
     var msg = nacl.randomBytes(num);
     var h = nacl.hash(msg);
-    var hexH = (new Buffer(h)).toString('hex');
-    chash(new Buffer(msg), function(hexCH) {
-      t.equal(hexH, hexCH, 'hashes should be equal');
+    var hexH = new Buffer(h).toString("hex");
+    chash(new Buffer(msg), function (hexCH) {
+      t.equal(hexH, hexCH, "hashes should be equal");
       if (num >= 1000) {
         t.end();
         return;
       }
-      check(num+1);
+      check(num + 1);
     });
   }
 
